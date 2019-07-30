3602417ae0cf9fd9ed9c0a20d29ca47108da9136
guzhenhua <707016185@qq.com>
2019-07-31 00:01:17 +0800

    2-6 使用title_contains检查页面是否正确

--- start_browser.py
+++ start_browser.py
@@ -1,3 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+import time
 driver=webdriver.Chrome()
-driver.get("https://www.baidu.com/")
+# driver=webdriver.edge()
+driver.get("http://www.5itest.cn/register")
+time.sleep(3)
+print(EC.title_contains("注册"))
