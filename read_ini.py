ab8ae72331ec3ccdfd86a0559f7751d01bfb404e
guzhenhua <707016185@qq.com>
2019-08-04 11:23:02 +0800

    2-20 重构封装读取配置文件方法

--- read_ini.py
+++ read_ini.py
@@ -1,5 +1,18 @@
 import configparser
-cf=configparser.ConfigParser()
-cf.read("E:\gu_python3_selenium\LocalElement.ini")
-string=cf.get("RegisterElement","user_email")
-print(string.split("<")[1])
+class Read_ini(object):
+    def __init__(self,file_name=None,node=None):
+        if file_name==None:
+            file_name="E:\gu_python3_selenium\LocalElement.ini"
+        if node==None:
+            node="RegisterElement"
+        self.node=node
+        self.cf=self.load_ini(file_name)
+    def load_ini(self,file_name):
+        cf = configparser.ConfigParser()
+        cf.read(file_name)
+        return cf
+    def get_value(self,key):
+        return  self.cf.get(self.node,key)
+if __name__=="__main__":
+    read_ini=Read_ini()
+    print(read_ini.get_value("user_nickname"))
